c76fc315e8636c09525887e31ca31a6fcbfd11cf
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-03-10 22:58:14 +0000

    login/sessionStorage working

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 console.log('Mongodb-frontend-practice');
 
 $( document ).ready(function() {
+
+if(sessionStorage['userName']) {
+  console.log('You are logged In');
+} else {
+    console.log('Please log In');
+}
+
   $('body').click(function(){
-    $(this).css('background','pink');
+    //$(this).css('background','pink');
   }); //jquery testing
 
 $('#loginForm').hide();
+$('#logoutBtn').hide();
 $('#adminPage').hide();
+$('#registerForm').hide();
+
 $('#adminBtn').click(function(){
   $('#adminPage').show();
   $('#homePage').hide();
@@ -19,6 +29,10 @@ $('#homeBtn').click(function(){
 
 $('#loginBtn').click(function(){
   $('#loginForm').show();
+});
+
+$('#registerBtn').click(function(){
+  $('#registerForm').show();
 });
 
 //get url and port from config.json
@@ -76,6 +90,39 @@ $('#viewProductsBtn').click(function(){
       });//View Products Button
 
 
+  $('#loginForm').submit(function(){
+    event.preventDefault();
+    let username = $('#username').val();
+    let password = $('#password').val();
+    console.log(username, password);
+
+    $.ajax({
+        url : `${url}/loginUser`,
+        type: 'POST',
+        data: {
+          username : username,
+          password : password
+        },
+        success : function(loginData){
+          console.log(loginData);
+          if(loginData === 'User not found, Please register') {
+            alert('Register please');
+          } else {
+            sessionStorage.setItem('userId', loginData['_id']);
+            sessionStorage.setItem('userName', loginData['username']);
+            sessionStorage.setItem('userEmail', loginData['email']);
+            console.log(sessionStorage);
+            console.log('You are logged In');
+          }
+
+        }, //Success
+        error: function() {
+          console.log('error: can not call api');
+        } //Error
+
+    }); //Ajax
+
+  }); //Submit function for login form.
 
 
 
